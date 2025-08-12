96907478fb351aae88a0ddd325781b81bf0f8675
Rayan Meziani <rayanmeziani95@outlook.com>
2025-08-12 14:14:30 +0200

    first commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,103 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <div className="bg-[#D7D5CA] relative">
+      <div className="grid place-self-center mx-6 md:mx-12 gap-y-12 md:gap-y-24 grid-cols-1 h-full md:grid-cols-2 py-6 mb-12 md:mb-0 md:py-12">
+        <div className="col-span-full">
+          <Image
+            src="/footer/logo.svg"
+            alt="footer-logo"
+            className="w-full h-auto object-contain"
+            height={250}
+            width={1600}
+            unoptimized
+            priority
+            draggable={false}
+          />
+        </div>
+        <div className="w-full justify-items-start">
+          <h1 className="text-black text-[36px] uppercase leading-[1.1] font-extrabold">
+            Start <br /> Building
+          </h1>
+        </div>
+        <div className="w-full flex flex-row justify-between md:justify-around justify-items-center">
+          <div className="flex text-[24px] flex-col space-y-4">
+            <Link
+              href="/partners"
+              className="transition-colors font-medium text-[20px] uppercase duration-300 hover:text-[#0B35F1]"
+            >
+              Partners
+            </Link>
+            <Link
+              href="https://docs.elizaos.ai"
+              className="transition-colors font-medium text-[20px] uppercase duration-300 hover:text-[#0B35F1]"
+              target="_blank"
+            >
+              Docs
+            </Link>
+            <Link
+              href="https://github.com/elizaos"
+              className="transition-colors font-medium text-[20px] uppercase duration-300 hover:text-[#0B35F1]"
+              target="_blank"
+            >
+              Github
+            </Link>
+            <Link
+              href="mailto:inquiries@elizalabs.ai"
+              className="transition-colors font-medium text-[20px] uppercase duration-300 hover:text-[#0B35F1]"
+            >
+              Contact
+            </Link>
+          </div>
+          <div className="flex text-[24px] flex-col space-y-4">
+            <Link
+              target="_blank"
+              href="https://github.com/elizaos"
+              className="flex items-center space-x-2"
+            >
+              <Image
+                src="/footer/github-icon.svg"
+                alt="Github"
+                width={36}
+                height={36}
+                draggable={false}
+              />
+              <h1>Github</h1>
+            </Link>
+
+            <Link
+              target="_blank"
+              className="flex items-center space-x-2"
+              href="https://discord.com/invite/tgCCVF9vEa"
+            >
+              <Image
+                src="/footer/discord-icon.svg"
+                alt="Contact"
+                width={36}
+                height={36}
+                draggable={false}
+              />
+              <h1>Discord</h1>
+            </Link>
+            <Link
+              className="flex items-center space-x-2"
+              href="https://x.com/elizaos"
+              target="_blank"
+            >
+              <Image
+                src="/footer/x-icon.svg"
+                alt="Docs"
+                width={36}
+                height={36}
+                draggable={false}
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[url('/footer/checker-bar.svg')] bg-repeat w-full h-10 mt-8" />
+    </div>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className="flex flex-row justify-between">
+      <Image
+        width={120}
+        height={20}
+        src="/header/header-logo.svg"
+        alt="header-logo"
+      />
+      <div className="mr-0 lg:mr-55 relative -mt-8">
+        <Image
+          src="/header/eliza-icon-text.png"
+          priority
+          unoptimized
+          height={50}
+          width={154}
+          alt="header-eliza-icon"
+        />
+      </div>
+    </div>
+  );
+}
