04c05593dd2001fd5ddd1ee66279f2b03b21a22c
Rayan Meziani <rayanmeziani95@outlook.com>
2025-08-14 11:53:29 +0200

    correct acacordion

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -2,9 +2,10 @@
 
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
-import { ChevronDownIcon } from "lucide-react"
+import { useState } from "react"
 
 import { cn } from "@/lib/utils"
+import { CircleMinusIcon, CirclePlusIcon } from "lucide-react"
 
 function Accordion({
   ...props
@@ -30,22 +31,30 @@ function AccordionTrigger({
   children,
   ...props
 }: React.ComponentProps<typeof AccordionPrimitive.Trigger>) {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <AccordionPrimitive.Header className="flex">
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
+        onClick={() => setIsOpen((prev) => !prev)}
         className={cn(
-          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none hover:underline focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>svg]:rotate-180",
+          "focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 items-start justify-between gap-4 rounded-md py-4 text-left text-sm font-medium transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50",
           className
         )}
         {...props}
       >
         {children}
-        <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
+        {isOpen ? (
+          <CircleMinusIcon className="text-[#3457F7] pointer-events-none size-7 shrink-0 translate-y-0.5 transition-transform duration-200" />
+        ) : (
+          <CirclePlusIcon className="text-[#3457F7] pointer-events-none size-7 shrink-0 translate-y-0.5 transition-transform duration-200" />
+        )}
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
   )
 }
+
 
 function AccordionContent({
   className,
