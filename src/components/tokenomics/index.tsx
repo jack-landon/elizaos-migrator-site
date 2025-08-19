c84570335a9317fbbb28c412c60340933ec8da3b
Rayan Meziani <rayanmeziani95@outlook.com>
2025-08-19 23:42:19 +0200

    image plugin for chart

--- src/components/tokenomics/index.tsx
+++ src/components/tokenomics/index.tsx
@@ -2,7 +2,7 @@
 
 import { useRef, useState } from "react";
 import { Doughnut } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip } from "chart.js";
+import { Chart as ChartJS, ArcElement, Tooltip, Chart } from "chart.js";
 
 ChartJS.register(ArcElement, Tooltip);
 
@@ -45,10 +45,28 @@ export default function TokenOmics() {
     ],
   };
 
+  const centerImagePlugin = {
+    id: "centerImage",
+    afterDraw: (chart: Chart) => {
+      const { ctx, chartArea } = chart;
+      const image = new Image();
+      image.src = "/chart/eliza-pictogram.png";
+      const x = (chartArea.left + chartArea.right) / 2;
+      const y = (chartArea.top + chartArea.bottom) / 2;
+      const size = 255;
+
+      image.onload = () => {
+        ctx.save();
+        ctx.drawImage(image, x - size / 2, y - size / 2, size, size);
+        ctx.restore();
+      };
+    },
+  };
+
   const options: any = {
     cutout: "60%",
     plugins: {
-      legend: { display: false }, // disable built-in legend
+      legend: { display: false },
     },
     animation: false,
   };
@@ -67,30 +85,38 @@ export default function TokenOmics() {
         </p>
       </div>
 
-      {/* Chart + Legend */}
+      {/* Chart */}
       <div className="grid col-span-1 items-center justify-start px-12 lg:px-0">
-        {/* Chart */}
-        <div className="h-[700px]  w-[700px]">
-          <Doughnut ref={chartRef} data={data} options={options} />
+        <div className="h-[700px] w-[700px]">
+          <Doughnut
+            ref={chartRef}
+            data={data}
+            options={options}
+            plugins={[centerImagePlugin]}
+          />
         </div>
-
-        {/* Custom Legend (This is the key change) */}
       </div>
+
+      {/* Custom Legend */}
       <div className="place-items-center grid grid-cols-1">
         <div className="w-full max-w-[480px] flex flex-col gap-8">
           {data.labels.map((label, i) => (
             <div
               key={i}
-              className="flex items-center border-b-2 border-[#0B35F1] space-x-2 cursor-pointer"
+              className="flex items-center border-b-2 border-[#0B35F1] cursor-pointer"
               onMouseEnter={() => setHoverIndex(i)}
               onMouseLeave={() => setHoverIndex(null)}
             >
+              <span className="text-[21px] font-normal text-white w-[60px]">
+                {data.datasets[0].data[i]}%
+              </span>
+
               <span
-                className={`text-[28px] mb-3 font-normal ${
-                  hoverIndex === i ? "text-white" : "text-white text-[28px]"
+                className={`text-[28px] font-normal flex-1 text-start ${
+                  hoverIndex === i ? "text-white" : "text-white"
                 }`}
               >
-                {data.datasets[0].data[i]}% - {label}
+                {label}
               </span>
             </div>
           ))}
