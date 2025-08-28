d0445f0b5c61e00f085c1c50556a26ae4c383082
SuchJitter <jettenmostafa2@gmail.com>
2025-08-29 00:25:20 +0200

    add animation

--- src/app/migrate/components/swap-selector.tsx
+++ src/app/migrate/components/swap-selector.tsx
@@ -15,30 +15,49 @@ export default function SwapSelector({
   selected,
 }: SwapSelectorProps) {
   return (
-    <div className="flex gap-5 pb-4">
+    <div className="flex gap-5 pb-4 relative">
       <button
-        className="justify-start flex flex-col align-start space-y-1"
+        className="justify-start flex flex-col align-start space-y-1 group"
         onClick={() => onChange(SwapType.Migrate)}
       >
         <div
           className={twMerge(
-            "border-b-3 border-b-transparent hover:cursor-pointer transition-all w-20",
+            "border-b-3 border-b-transparent hover:cursor-pointer transition-all duration-300 ease-in-out w-20",
             selected === SwapType.Migrate && "border-b-[#0B35F1]"
           )}
         />
-        <p className="font-normal text-left text-xl text-[#0B35F1]">Migrate</p>
+        <p
+          className={twMerge(
+            "font-normal text-left text-xl transition-all duration-300 ease-in-out",
+            selected === SwapType.Migrate
+              ? "text-[#0B35F1] scale-105"
+              : "text-[#616C99] hover:text-[#0B35F1] group-hover:scale-102"
+          )}
+        >
+          Migrate
+        </p>
       </button>
+
       <button
         onClick={() => onChange(SwapType.Bridge)}
-        className="justify-start flex flex-col align-start space-y-1"
+        className="justify-start flex flex-col align-start space-y-1 group"
       >
         <div
           className={twMerge(
-            "border-b-3 border-b-transparent hover:cursor-pointer transition-all w-20",
+            "border-b-3 border-b-transparent hover:cursor-pointer transition-all duration-300 ease-in-out w-20",
             selected === SwapType.Bridge && "border-b-[#0B35F1]"
           )}
         />
-        <p className="font-normal text-left text-xl text-[#0B35F1]">Bridge</p>
+        <p
+          className={twMerge(
+            "font-normal text-left text-xl transition-all duration-300 ease-in-out",
+            selected === SwapType.Bridge
+              ? "text-[#0B35F1] scale-105"
+              : "text-[#616C99] hover:text-[#0B35F1] group-hover:scale-102"
+          )}
+        >
+          Bridge
+        </p>
       </button>
     </div>
   );
