6c9c7e2a43af8bc8789ff24bacaaa741b58fe6c7
Rayan Meziani <rayanmeziani95@outlook.com>
2025-08-28 15:42:26 +0200

    further progress on expansion section

--- src/components/expansion.tsx
+++ src/components/expansion.tsx
@@ -1,8 +1,32 @@
+import Image from "next/image";
+
 export default function Expansion() {
+  const images = [
+    "/expansion/base.png",
+    "/expansion/eth.png",
+    "/expansion/eliza.png",
+    "/expansion/hyper.png",
+    "/expansion/solana.png",
+  ];
+
   return (
-    <div className="relative h-[85vh] w-full bg-[#0B35F1]">
+    <div className="relative h-[75vh] lg:max-h-[85vh] lg:h-[85vh] w-full overflow-hidden">
+      {/* Background video */}
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute top-0 left-0 w-full h-full object-cover z-0 brightness-75 grayscale-100"
+      >
+        <source src="/expansion/solar.mp4" type="video/mp4" />
+      </video>
+
+      {/* Color overlay */}
+      <div className="absolute inset-0 bg-[#0B35F1]/20 z-10" />
+
       {/* Content */}
-      <div className="relative h-full grid grid-cols-1 xl:grid-cols-2">
+      <div className="relative z-20 flex flex-col h-full">
         <div className="grid h-fit justify-between p-6 lg:p-12 col-span-1">
           <p className="text-white uppercase text-[24px] font-medium">
             expansion
@@ -15,6 +39,22 @@ export default function Expansion() {
             even back to Solana — and choose the ecosystem that fits your
             strategy.
           </p>
+        </div>
+
+        <div className="flex justify-center items-center md:mt-0 lg:mt-24 lg:items-start h-full px-12">
+          <div className="flex items-center w-full">
+            {images.map((src, i) => (
+              <div key={i} className="flex-1 flex justify-center">
+                <Image
+                  src={src}
+                  alt={`expansion-${i + 1}`}
+                  width={src.includes("eliza.png") ? 400 : 100}
+                  height={src.includes("eliza.png") ? 400 : 100}
+                  draggable={false}
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
