5e7754bc5da2a46ca704917c300ad278f722589e
Rayan Meziani <rayanmeziani95@outlook.com>
2025-09-02 13:39:50 +0200

    created banner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,28 @@
 import Expansion from "@/components/expansion";
 import Faq from "@/components/faq";
 import Hero from "@/components/hero";
+import HeroBar from "@/components/hero-bar";
 import HowItWorks from "@/components/how-it-works";
 import BackedBySwarm from "@/components/swarm";
 import TokenOmics from "@/components/tokenomics";
 
 export default function Home() {
   return (
-    <div>
+    <div className="relative">
+      {/* Hero section wrapper */}
       <Hero />
-      <BackedBySwarm />
-      <HowItWorks />
-      <Expansion />
-      <TokenOmics />
-      <Faq />
+
+      {/* HeroBar over Hero and Header, only on lg and up */}
+      <div className="absolute -top-27 right-24 lg:block hidden">
+        <HeroBar />
+      </div>
+      <div className="z-20">
+        <BackedBySwarm />
+        <HowItWorks />
+        <Expansion />
+        <TokenOmics />
+        <Faq />
+      </div>
     </div>
   );
 }
